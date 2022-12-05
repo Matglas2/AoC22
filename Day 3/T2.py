243d8f4c5969c5e22b729d9155b5e4b92b41b1c1
Thijs Erends <matglas2@gmail.com>
2022-12-05 20:28:36 +0100

    C0mments

--- Day 3/T2.py
+++ Day 3/T2.py
@@ -7,17 +7,20 @@ groupsize = 3
 
 backpackgroups = []
 
+#group in sets of 3
 for index in range(0, len(backpacks), groupsize):
     backpackgroups.append(backpacks[index:index+groupsize])
 
+#find and score letters. needs to care about groupsize in the future
 for backpackgroup in backpackgroups:
     founditems = []
 
-    #find and score letters. needs to care about groupsize in the future
+    #Find common letters in group 1 and 2
     for char in backpackgroup[0]:
         if backpackgroup[1].find(char) != -1:
             founditems.append(char)
     
+    #use distinct list to find last match and cancel as soon as its found
     for char in "".join(set(founditems)):
         if backpackgroup[2].find(char) != -1:
             score += alphabet.find(char) + 1
