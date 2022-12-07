7414db7849796b801ec1fbdb82117d7ddf65dbf3
Thijs Erends <matglas2@gmail.com>
2022-12-07 19:10:30 +0100

    layout

--- Day 5/T1-2.py
+++ Day 5/T1-2.py
@@ -14,14 +14,14 @@ while not inputdata[i].startswith(" 1"):
 # determine the amount of stacks in the input by counting the stack id's
 stackids = re.findall("(\d+)", inputdata[i])
 
-#prepare a array with the same anout of subarrays (all empty)
+# prepare a array with the same anout of subarrays (all empty)
 stacks =  [[] for x in range(len(stackids))]
 
 for blockline in blockinput:
-    #regex my way out of interpreting the input blocks
+    # regex my way out of interpreting the input blocks
     blocks = re.findall("(\w|\s{4})", blockline)
     
-    #Put block in order in to the precreatred arrays
+    # put block in order in to the precreatred arrays
     stackid = 0
     for block in blocks:
         if block != "    ":
@@ -29,7 +29,7 @@ for blockline in blockinput:
         stackid += 1
 
 for actionline in inputdata[i +2:]:
-    #parse the actions
+    # parse the actions
     action = re.findall("\d+",actionline)
 
     # move block to front of array
@@ -40,7 +40,7 @@ for actionline in inputdata[i +2:]:
         #same order
         stacks[int(action[2]) -1].insert(0 + x, block)
 
-#find answer
+# find answer
 answer = ""
 for stack in stacks:
     answer += (stack[0])
